3d15c18ed210d1fb927965504a673ff0db68c272
FrancoisLaptopDell <FrancoisLaptopDell@da31bc54-76c1-3b4e-8f42-40e98a57958f>
2012-12-18 23:28:55 +0000

    Just added required dependencies (and adapted code) to work with new AutoUpdating.cs.
    git-svn-id: svn://localhost/testingbytearraydiff/trunk@22 da31bc54-76c1-3b4e-8f42-40e98a57958f

--- TestingByteArrayDiff/Program.cs
+++ TestingByteArrayDiff/Program.cs
@@ -19,12 +19,16 @@ namespace TestingByteArrayDiff
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Form mainForm = new Form1();
-			AutoUpdating.CheckForUpdates(
+			AutoUpdating.CheckForUpdates_ExceptionHandler(delegate
+			{
+				ThreadingInterop.UpdateGuiFromThread(mainForm, () => mainForm.Text += " (up to date version " + AutoUpdating.GetThisAppVersionString() + ")");
+			});
+			/*AutoUpdating.CheckForUpdates(
 				//AutoUpdatingForm.CheckForUpdates(
 				//exitApplicationAction: () => mainForm.Close(),
 				ActionIfUptoDate_Versionstring: versionstring => ThreadingInterop.UpdateGuiFromThread(mainForm, () => mainForm.Text += " (up to date version " + versionstring + ")"));//,
 				//ActionIfUnableToCheckForUpdates: errmsg =>ThreadingInterop.UpdateGuiFromThread(mainForm, () => mainForm.Text += " (" + errmsg + ")"),
-				//ShowModally: true);
+				//ShowModally: true);*/
 
 			typeof(Form).GetField("defaultIcon", BindingFlags.NonPublic | BindingFlags.Static)
 				.SetValue(null, new Icon(System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("TestingByteArrayDiff.app.ico")));
